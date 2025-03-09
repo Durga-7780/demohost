81fb988db26e4e1b5cf40c21b114b4cc111ff898
Durga-0778 <durgapragada204@gmail.com>
2025-03-09 15:31:30 +0530

    index js modified onclick

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var password = document.getElementById("password").value;
 
 console.log("username "+username+" password "+password);
 
-var btn=document.getElementById("btn")[0];
+var btn=document.getElementsByClassName("btn")[0];
 btn.onclick=()=>{
     window.location.href="home.html";
 }
